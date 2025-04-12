184940dc221de16e187d3a93eec1ebfff69eeb30
ramazandautbek <ramazandautbek04@gmail.com>
2025-04-12 23:59:25 +0500

    Добавить страницы для управления турами и middleware для проверки токена

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export function middleware(req : NextRequest){
+    const token = req.cookies.get('token')?.value
+
+    if(!token){
+        return NextResponse.redirect(new URL('login', req.url))
+    }
+
+    const payload = parseJwt(token)
+
+    if(payload.role !== 'admin'){
+        return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    return NextResponse.next()
+}
+
+function parseJwt(token: string){
+    try{
+        const base64 = token.split('.')[1]
+        const jsonPayload = Buffer.from(base64, 'base64').toString()
+        return JSON.parse(jsonPayload)
+    }catch(e){
+        console.log(e)
+        return {}
+    }
+}
+
+export const config = {
+    matcher: ['/admin/:path*'],
+}
