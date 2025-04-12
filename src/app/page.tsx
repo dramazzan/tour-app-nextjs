feef78c1fd3ae88232358dec0b4af17285b15ed6
ramazandautbek <ramazandautbek04@gmail.com>
2025-04-12 16:15:40 +0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="text-lg">Welcome to the Tour App!</p>
+      <p className="text-lg">This is a simple application to demonstrate the use of Next.js and Tailwind CSS.</p>
+      <p className="text-lg">Enjoy your stay!</p>
+    </>
+  );
+}
